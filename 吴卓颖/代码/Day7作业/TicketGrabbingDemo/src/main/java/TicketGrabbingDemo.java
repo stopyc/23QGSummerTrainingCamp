08eead9a2795268872172704f37270e0733a3931
Seeeecret <2633967055@qq.com>
2023-07-16 23:19:06 +0800

    feat(周记):周记提交

--- 吴卓颖/代码/Day7作业/TicketGrabbingDemo/src/main/java/TicketGrabbingDemo.java
+++ 吴卓颖/代码/Day7作业/TicketGrabbingDemo/src/main/java/TicketGrabbingDemo.java
@@ -1,6 +1,6 @@
 public class TicketGrabbingDemo {
     public static void main(String[] args) {
-        TicketMachine ticketMachine = new TicketMachine(100, 30);
+        TicketMachine ticketMachine = new TicketMachine(10, 50);
         ticketMachine.startGrabTicket();
     }
 }
